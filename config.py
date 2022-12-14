e21b4561ce21bd634a31f74f88430b12c1291b80
anttonibarbia <antton.ibarbia@gmail.com>
2022-12-14 09:28:35 +0100

    email alvaro

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ user_email = 'scraping.robot1@gmail.com'
 pass_email = 'znuwpyokuwsybkcy'
 
 # Emails a...
-recipients_email = ['antton.ibarbia@gmail.com']
+recipients_email = ['alvaro.olmedo.mir@gmail.com']
 
 # Lista de timeframes para las busquedas por email: ultimas 1h, 4h y 24h.
 timeframes_scraper_email = ['now 1-H', 'now 4-H', 'now 1-d']
